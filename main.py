115b12d403364ac6682983ff6f570c80d26a6a48
Jorge Moreno <jordmoreno@gmail.com>
2019-05-20 15:59:20 +0300

    Added some examples

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from utils.imageutils import img_to_binary
 
 if __name__=="__main__":
 
-    name="alex.jpg"
+    name="diegolau1.jpeg"
 
-    processed_img_path = img_to_binary(name, filter_size=11, threshold=100)
+    processed_img_path = img_to_binary(name, filter_size=11, threshold=200)
     print("Processed image succesfully. Image saved in "+processed_img_path)
