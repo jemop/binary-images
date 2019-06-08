5c0adfc69f5a787b70bc2350ba8d52f2fffc9a69
Jorge Moreno <jordmoreno@gmail.com>
2019-06-08 22:18:45 +0300

    Added lines

--- img2greygrid.py
+++ img2greygrid.py
@@ -7,6 +7,8 @@ from skimage.util.shape import view_as_windows
 ORIGINAL_IMAGES_PATH = os.path.join("data", "original_images")
 PROCESSED_IMAGES_PATH = os.path.join("data", "processed_images")
 
+LINE_COLOR=0    # 0 to 255
+
 
 def load_image_grayscale(name):
     """
@@ -63,7 +65,21 @@ def apply_filter(img, thresholds, filter_size=7, padding=3, stride=7):
     return values*255/len(thresholds)
 
 
-def img_to_ncolors(name, thresholds, filter_size=7, keep_dims=True):
+def draw_lines_grayscale(image, filter_size):
+    pixel_size = (int(image.shape[1]/filter_size), int(image.shape[1]/filter_size))
+
+    for i in range(image.shape[0]):
+        if i % filter_size == 0:
+            image[i, :] = np.ones(image.shape[1])*LINE_COLOR
+
+    for j in range(image.shape[1]):
+        if j % filter_size == 0:
+            image[:, j] = np.ones(image.shape[0])*LINE_COLOR
+
+    return image
+
+
+def img_to_ncolors(name, thresholds, filter_size=7, draw_lines=True):
     """
     Loads an image, apply the desired filter, and saves it in the processed images folder, returning its path.
     :param thresholds:
@@ -77,8 +93,12 @@ def img_to_ncolors(name, thresholds, filter_size=7, keep_dims=True):
     processed_img = apply_filter(img, thresholds, filter_size=filter_size, padding=int(filter_size/2),
                                  stride=filter_size)
 
-    if keep_dims:
-        processed_img = cv2.resize(processed_img, dsize=(img.shape[1], img.shape[0]), interpolation=cv2.INTER_NEAREST)
+    processed_img = cv2.resize(processed_img, dsize=(processed_img.shape[1]*filter_size,
+                                                     processed_img.shape[0]*filter_size),
+                               interpolation=cv2.INTER_NEAREST)
+
+    if draw_lines:
+        processed_img = draw_lines_grayscale(processed_img, filter_size)
 
     return save_processed_image(name, processed_img, prefix=str(len(thresholds)+1)+"colors_")
 
@@ -86,7 +106,7 @@ def img_to_ncolors(name, thresholds, filter_size=7, keep_dims=True):
 if __name__=="__main__":
 
     # File must be located in data/original_images/
-    name="girl_dancing.jpg"
+    name = "girl_dancing.jpg"
 
-    processed_ncolors_path = img_to_ncolors(name, thresholds=[50, 100, 150, 200], filter_size=15)
+    processed_ncolors_path = img_to_ncolors(name, thresholds=[50, 100, 150, 200, 230], filter_size=15)
     print("Processed images succesfully.")
